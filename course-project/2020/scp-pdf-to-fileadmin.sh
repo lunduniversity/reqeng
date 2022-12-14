40a2c49619173a6640fe04a751b1c45e8485db65
bjornregnell <bjorn.regnell@cs.lth.se>
2022-12-14 12:36:34 +0100

    cp old project descr from closed git server

---
+++ course-project/2020/scp-pdf-to-fileadmin.sh
@@ -0,0 +1,4 @@
+YEAR=2020
+LUCAT=tts-bre
+echo "scp main.pdf $LUCAT@web.cs.lth.se:/Websites/Fileadmin/cs/Education/ETSN15/proj$YEAR.pdf"
+scp main.pdf $LUCAT@web.cs.lth.se:/Websites/Fileadmin/cs/Education/ETSN15/proj$YEAR.pdf
---
+++ course-project/2021/scp-pdf-to-fileadmin.sh
@@ -0,0 +1,4 @@
+YEAR=2021
+LUCAT=tts-bre
+echo "scp main.pdf $LUCAT@fileadmin.cs.lth.se:/Websites/Fileadmin/krav/proj$YEAR.pdf"
+scp main.pdf $LUCAT@fileadmin.cs.lth.se:/Websites/Fileadmin/krav/proj$YEAR.pdf
